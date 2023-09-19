11e672e575679aa4b2e53ad392dca9c99f0ca738
vivian_pc <carohinca1997@gmail.com>
2023-09-19 09:44:16 -0500

    creacion de template base y templates cuentas,clientes

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+'''Conexión a bd'''
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='retohuila'
+mysql=MySQL(app)
+
+'''Settings'''
+app.secret_key='mysecretkey'
+
 
 @app.route('/')
 def index():
